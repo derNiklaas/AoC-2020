6fbd22def961ee8c7b3e1c5e1ee2da2a7c7c439b
Niklas <derNiklaas@users.noreply.github.com>
2020-12-12 18:25:42 +0100

    Added Day 12

--- src/de/derniklaas/aoc2020/Main.java
+++ src/de/derniklaas/aoc2020/Main.java
@@ -11,7 +11,7 @@ public class Main {
     public static boolean debug = false;
 
     public static void main(String[] args) {
-        int day = 11;
+        int day = 12;
         if (args.length == 1) {
             try {
                 day = Integer.parseInt(args[0]);
@@ -77,6 +77,10 @@ public class Main {
             }
             case 11: {
                 new Day11(content).printAnswers();
+                break;
+            }
+            case 12: {
+                new Day12(content).printAnswers();
                 break;
             }
             default: {
---
+++ src/de/derniklaas/aoc2020/days/Day12.java
@@ -0,0 +1,196 @@
+package de.derniklaas.aoc2020.days;
+
+public class Day12 {
+
+    private final String[] input;
+
+    public Day12(String[] input) {
+        this.input = input;
+    }
+
+    public void printAnswers() {
+        printA();
+        printB();
+    }
+
+    private void printA() {
+        int direction = 90;
+        int x = 0;
+        int y = 0;
+        for (String part : input) {
+            char operation = part.charAt(0);
+            int amount = Integer.parseInt(part.substring(1));
+            switch (operation) {
+                case 'N': {
+                    y += amount;
+                    break;
+                }
+                case 'S': {
+                    y -= amount;
+                    break;
+                }
+                case 'E': {
+                    x += amount;
+                    break;
+                }
+                case 'W': {
+                    x -= amount;
+                    break;
+                }
+                case 'L': {
+                    direction -= amount;
+                    while (direction < 0) {
+                        direction += 360;
+                    }
+                    break;
+                }
+                case 'R': {
+                    direction += amount;
+                    while (direction >= 360) {
+                        direction -= 360;
+                    }
+                    break;
+                }
+                case 'F': {
+                    if (direction == 0) {
+                        y += amount;
+                    } else if (direction == 90) {
+                        x += amount;
+                    } else if (direction == 180) {
+                        y -= amount;
+                    } else if (direction == 270) {
+                        x -= amount;
+                    }
+                    break;
+                }
+            }
+        }
+        int sum = Math.abs(x) + Math.abs(y);
+        System.out.println("[Day 12/A] " + sum);
+    }
+
+    private void printB() {
+        int waypointY = 1;
+        int waypointX = 10;
+        int direction = 90;
+        int x = 0;
+        int y = 0;
+        for (String part : input) {
+            char operation = part.charAt(0);
+            int amount = Integer.parseInt(part.substring(1));
+            switch (operation) {
+                case 'N': {
+                    waypointY += amount;
+                    break;
+                }
+                case 'S': {
+                    waypointY -= amount;
+                    break;
+                }
+                case 'E': {
+                    waypointX += amount;
+                    break;
+                }
+                case 'W': {
+                    waypointX -= amount;
+                    break;
+                }
+                case 'L': {
+                    int oldDirection = direction;
+                    direction -= amount;
+                    while (direction < 0) {
+                        direction += 360;
+                    }
+                    int tempX = calcWaypointDirectionX(waypointX, waypointY, direction, oldDirection);
+                    int tempY = calcWaypointDirectionY(waypointX, waypointY, direction, oldDirection);
+
+                    waypointX = tempX;
+                    waypointY = tempY;
+                    break;
+                }
+                case 'R': {
+                    int oldDirection = direction;
+                    direction += amount;
+                    while (direction >= 360) {
+                        direction -= 360;
+                    }
+                    int tempX = calcWaypointDirectionX(waypointX, waypointY, direction, oldDirection);
+                    int tempY = calcWaypointDirectionY(waypointX, waypointY, direction, oldDirection);
+
+                    waypointX = tempX;
+                    waypointY = tempY;
+                    break;
+                }
+                case 'F': {
+                    x += amount * waypointX;
+                    y += amount * waypointY;
+                    break;
+                }
+            }
+            //System.out.println("x= " + x + " y= " + y + " facing= " + direction + " waypointX= " + waypointX + " waypointY= " + waypointY);
+        }
+        int sum = Math.abs(x) + Math.abs(y);
+        System.out.println("[Day 12/B] " + sum);
+    }
+
+    private int calcWaypointDirectionX(int waypointX, int waypointY, int direction, int oldDirection) {
+        switch (direction) {
+            case 0: {
+                if (oldDirection == 0) return waypointX;
+                if (oldDirection == 180) return -waypointX;
+                if (oldDirection == 90) return -waypointY;
+                return waypointY;
+            }
+            case 90: {
+                if (oldDirection == 0) return waypointY;
+                if (oldDirection == 90) return waypointX;
+                if (oldDirection == 270) return -waypointX;
+                return -waypointY;
+            }
+            case 180: {
+                if (oldDirection == 0) return -waypointX;
+                if (oldDirection == 180) return waypointX;
+                if (oldDirection == 90) return waypointY;
+                return -waypointY;
+            }
+            case 270: {
+                if (oldDirection == 0) return -waypointY;
+                if (oldDirection == 90) return -waypointX;
+                if (oldDirection == 270) return waypointX;
+                return waypointY;
+            }
+        }
+        throw new IllegalStateException("Something went wrong.");
+    }
+
+
+    private int calcWaypointDirectionY(int waypointX, int waypointY, int direction, int oldDirection) {
+        switch (direction) {
+            case 0: {
+                if (oldDirection == 0) return waypointY;
+                if (oldDirection == 90) return waypointX;
+                if (oldDirection == 180) return -waypointY;
+                return -waypointX;
+            }
+            case 180: {
+                if (oldDirection == 0) return -waypointY;
+                if (oldDirection == 90) return -waypointX;
+                if (oldDirection == 180) return waypointY;
+                return waypointX;
+            }
+            case 90: {
+                if (oldDirection == 0) return -waypointX;
+                if (oldDirection == 90) return waypointY;
+                if (oldDirection == 270) return -waypointY;
+                return waypointX;
+            }
+            case 270: {
+                if (oldDirection == 0) return waypointX;
+                if (oldDirection == 90) return -waypointY;
+                if (oldDirection == 270) return waypointY;
+                return -waypointX;
+            }
+        }
+        throw new IllegalStateException("Something went wrong.");
+    }
+}
