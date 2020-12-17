fa5b120dc75a09e6de6c9d003661782ba99f5193
Niklas <derNiklaas@users.noreply.github.com>
2020-12-17 14:15:44 +0100

    Added noeppi_noeppi to the readme table.

--- src/de/derniklaas/aoc2020/days/Day17.java
+++ src/de/derniklaas/aoc2020/days/Day17.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class Day17 {
 
-    private Set<Pos4D> initialState;
+    private final Set<Pos4D> initialState;
 
     public Day17(String[] input) {
         initialState = new HashSet<>();
